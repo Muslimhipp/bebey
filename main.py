363336979f1ab3d0b15e4aa78141c20119cefaac
Caribou <118128295+Muslimhipp@users.noreply.github.com>
2023-03-17 21:11:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from io import BytesIO
 
 app = Flask(__name__)
 
+@app.route("/")
+def root():
+   return '''<form action="/qr" method="GET">
+      <input type="text" name="msg" placeholder="Enter text"/>
+      <input type="submit" value="QR-code"/>
+   </form>'''
+
+
 @app.route("/qr")
 def qr():
    msg = request.args.get('msg')
@@ -20,4 +28,3 @@ def qr():
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0')
-
